2e0149acaa53ee113c1c2115c64aedbda48ab00d
Shailesh Rajput Personal <meetshaileshrajput@gmail.com>
2026-09-03 22:22:22 +0530

    Untrack build artifacts

--- android/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_dopashift_app_DopaShiftApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.dopashift.app.DopaShiftApplication"
-)
-public final class _com_dopashift_app_DopaShiftApplication {
-}
